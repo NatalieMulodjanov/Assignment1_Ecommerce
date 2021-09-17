0f25902333bfb4fcf78d3e4f4cbfb282471f0c78
CryoTechnic <cryogen65@gmail.com>
2021-09-17 15:14:38 -0400

    Implemented models for address & person

---
+++ app/models/Address.php
@@ -0,0 +1,99 @@
+<?php 
+
+namespace app\models;
+
+class Address extends \app\core\Model {
+    public $description;
+    public $street;
+    public $city;
+    public $province_state;
+    public $postal_zip;
+    public $country_code;
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+    
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+
+    public function getStreet() {
+        return $this->street;
+    }
+
+    public function setStreet($street) {
+        $this->street = $street;
+    }
+
+    public function getCity() {
+        return $this->city;
+    }
+
+    public function setCity($city) {
+        $this->city = $city;
+    }
+
+    public function getProvinceState() {
+        return $this->province_state;
+    }
+
+    public function setProvinceState($province_state) {
+        $this->province_state = $province_state;
+    }
+
+    public function getPostalZip() {
+        return $this->postal_zip;
+    }
+
+    public function setPostalZip($postal_zip) {
+        $this->postal_zip = $postal_zip;
+    }
+
+    public function getCountryCode() {
+        return $this->country_code;
+    }
+
+    public function setCountryCode($country_code) {
+        $this->country_code = $country_code;
+    }
+
+    public function getAll($person_id) { // get all addresses for a person
+        $SQL = 'SELECT * FROM address_information WHERE person_id = :person_id';
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute(array(':person_id' => $person_id));
+        $STMT->setFetchMode(\PDO::FETCH_CLASS, '\app\models\Address');
+        return $STMT->fetchAll();
+    }
+
+    public function get($address_id) { // get a single address
+        $SQL = 'SELECT * FROM address_information WHERE address_id = :address_id';
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute(array(':address_id' => $address_id));
+        $STMT->setFetchMode(\PDO::FETCH_CLASS, '\app\models\Address');
+        return $STMT->fetch();
+    }  
+
+    public function insert() { // insert a new address
+        $SQL = 'INSERT INTO address_information (person_id, description, street, city, province_state, postal_zip, country_code) VALUES (:person_id, :description, :street, :city, :province_state, :postal_zip, :country_code)';
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute(array(':person_id' => $this->person_id, ':description' => $this->description, ':street' => $this->street, ':city' => $this->city, ':province_state' => $this->province_state, ':postal_zip' => $this->postal_zip, ':country_code' => $this->country_code));
+    } 
+
+    public function update() { // update an existing address
+        $SQL = 'UPDATE `address_information` SET `person_id` = :person_id, `description` = :description, `street` = :street, `city` = :city, `province_state` = :province_state, `postal_zip` = :postal_zip, `country_code` = :country_code WHERE address_id = :address_id';
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute(array(':person_id' => $this->person_id, ':description' => $this->description, ':street' => $this->street, ':city' => $this->city, ':province_state' => $this->province_state, ':postal_zip' => $this->postal_zip, ':country_code' => $this->country_code, ':address_id' => $this->address_id));    
+    } 
+
+    public function delete($address_id) { // delete an existing address
+        $SQL = 'DELETE FROM address_information WHERE address_id = :address_id';
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute(array(':address_id' => $address_id));
+    }  
+}
+?>
--- app/models/Person.php
+++ app/models/Person.php
@@ -6,66 +6,68 @@ class Person extends \app\core\Model {
     public $f_name;
     public $l_name;
     public $notes;
+
+
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function get_f_name() {
+        return $this->f_name;
+    }
+    
+    public function set_f_name($f_name) {
+        $this->f_name = $f_name;
+    }
+    
+    public function get_l_name() {
+        return $this->l_name;
+    }
+    
+    public function set_l_name($l_name) {
+        $this->l_name = $l_name;
+    }
+    
+    public function get_notes() {
+        return $this->notes;
+    }
+    
+    public function set_notes($notes) {
+        $this->notes = $notes;
+    }
+    
+    public function getAll() {
+        $SQL = "SELECT * FROM person_information";
+        $STMT = self::$_connection->query($SQL);
+        $STMT->setFetchMode(\PDO::FETCH_CLASS, 'app\\models\\Person'
+        return $STMT->fetchAll(); 
+    }
+    
+    // 
+    public function get($person_id) {
+        $SQL = "SELECT * FROM person_information WHERE person_id = :person_id";
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute(array(':person_id' => $person_id));
+        $STMT->setFetchMode(\PDO::FETCH_CLASS, 'app\\models\\Person');
+        return $STMT->fetch();
+    }
+    
+    public function insert(){
+        $SQL = "INSERT INTO person_information (f_name, l_name, notes) VALUES (:f_name, :l_name, :notes)";
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute(array(':f_name' => $this->f_name, ':l_name' => $this->l_name, ':notes' => $this->notes));
+    }
+    
+    public function update($person_id) {
+        $SQL = "UPDATE person_information SET f_name = :f_name, l_name = :l_name, notes = :notes WHERE person_id = :person_id";
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute(array(':f_name' => $this->f_name, ':l_name' => $this->l_name, ':notes' => $this->notes, ':person_id' => $person_id));
+    } // $person_id is the primary key
+    
+    public function delete($person_id) {
+        $SQL = "DELETE FROM person_information WHERE person_id = :person_id";
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute(array(':person_id' => $person_id));
+    }// $person_id is the primary key
 }
-
-public function __construct() {
-    parent::__construct();
-}
-
-public function get_f_name() {
-    return $this->f_name;
-}
-
-public function set_f_name($f_name) {
-    $this->f_name = $f_name;
-}
-
-public function get_l_name() {
-    return $this->l_name;
-}
-
-public function set_l_name($l_name) {
-    $this->l_name = $l_name;
-}
-
-public function get_notes() {
-    return $this->notes;
-}
-
-public function set_notes($notes) {
-    $this->notes = $notes;
-}
-
-public function getAll() {
-    $SQL = "SELECT * FROM person_information";
-    $STMT = self::$_connection->query($SQL);
-    $STMT->setFetchMode(\PDO::FETCH_CLASS, 'app\\models\\Person'
-    return $STMT->fetchAll(); 
-}
-
-public function get($person_id) {
-    $SQL = "SELECT * FROM person_information WHERE person_id = :person_id";
-    $STMT = self::$_connection->prepare($SQL);
-    $STMT->execute(array(':person_id' => $person_id));
-    $STMT->setFetchMode(\PDO::FETCH_CLASS, 'app\\models\\Person');
-    return $STMT->fetch();
-}
-
-public function insert(){
-    $SQL = "INSERT INTO person_information (f_name, l_name, notes) VALUES (:f_name, :l_name, :notes)";
-    $STMT = self::$_connection->prepare($SQL);
-    $STMT->execute(array(':f_name' => $this->f_name, ':l_name' => $this->l_name, ':notes' => $this->notes));
-}
-
-public function update($person_id) {
-    $SQL = "UPDATE person_information SET f_name = :f_name, l_name = :l_name, notes = :notes WHERE person_id = :person_id";
-    $STMT = self::$_connection->prepare($SQL);
-    $STMT->execute(array(':f_name' => $this->f_name, ':l_name' => $this->l_name, ':notes' => $this->notes, ':person_id' => $person_id));
-} // $person_id is the primary key
-
-public function delete($person_id) {
-    $SQL = "DELETE FROM person_information WHERE person_id = :person_id";
-    $STMT = self::$_connection->prepare($SQL);
-    $STMT->execute(array(':person_id' => $person_id));
-}// $person_id is the primary key
 ?>
