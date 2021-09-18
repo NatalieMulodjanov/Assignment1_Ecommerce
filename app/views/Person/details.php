bb2c044335c29ebc98fd4e589abcefed095cf3e9
CryoTechnic <cryogen65@gmail.com>
2021-09-18 18:18:23 -0400

    added details

---
+++ app/views/Person/details.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Person Details</title>
+    </head>
+    <body>
+        <h4>First Name: </h4><input disabled type="text" value="<?php echo $data->f_name; ?>"><br>
+        <h4>Last Name: </h4><input disabled type="text" value="<?php echo $data->l_name; ?>"><br>
+        <a href='/Person/index'>Back to Person list</a>
+    </body>
+</html>
+
--- app/views/Person/insert.php
+++
@@ -1,6 +0,0 @@
-<html>
-
-    <head>
-        <title>Add Person</title>
-    </head>
-</html>
