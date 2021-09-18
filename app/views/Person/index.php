750d14f6db5c7d149d3c9c5ab95ec9e8e03ad2f9
CryoTechnic <cryogen65@gmail.com>
2021-09-18 18:02:57 -0400

    Added index view for Person

---
+++ app/views/Person/index.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Person Index</title>
+    </head>
+    <body>
+        <a href='/Person/insert'>Add Person</a>
+        <table>
+            <tr><th>First Name</th><th>Last Name</th><th>Actions</th></tr>
+            <?php
+            foreach($data as $person){
+                echo "<tr>
+                        <td>$person->f_name</td>
+                        <td>$person->l_name</td>
+                        <td>$person->notes</td>
+                        <td>
+                            <a href='/Person/details/$person->person_id'>details</a> |
+                            <a href='/Person/edit/$person->person_id'>edit</a> |
+                            <a href='/Person/delete/$person->person_id'>delete</a> |
+                        </td>
+                    </tr>";
+                }
+             ?>
+        </table>
+    </body>
+</html>
