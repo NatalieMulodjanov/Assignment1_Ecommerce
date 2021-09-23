db94602b0a34824c179292ba8389c6553af0f1b7
CryoTechnic <cryogen65@gmail.com>
2021-09-23 00:19:34 -0400

    added some comments

--- app/controllers/addressController.php
+++ app/controllers/addressController.php
@@ -1,6 +1,12 @@
 <?php 
 namespace app\controllers;
 
+/**
+ *  AddressController class, which contains actions for the address page
+ *  Authors: Natalie Mulodjanov (1956449), Ron Friedman (1926133), Vanier College 2021
+ *  Date: 
+ *  This code is/will be published on GitHub. The license is GPLv3. Please do not remove this comment
+ */ 
 class AddressController extends \app\core\controller {
     public function index($person_id) {
         $myPerson = new \app\models\Person();
@@ -12,6 +18,10 @@ class AddressController extends \app\core\controller {
         $this->view('Address/index', $addresses);
     }
 
+    /**
+     * Add a new address to the database from form POST action
+     * @param int $person_id: the id of the person to which the address belongs
+     */
     public function insert($person_id) {
         if (isset ($_POST['submit'])) {
             $address = new \app\models\Address();
@@ -23,6 +33,7 @@ class AddressController extends \app\core\controller {
             $address->country = $_POST['country'];
             $address->insert();
 
+            // Redirect to the person's address page
             header("Location: /Address/index/$person_id");
         } else {
             $address = new \app\models\Address();
@@ -31,6 +42,10 @@ class AddressController extends \app\core\controller {
         }
     }
 
+    /**
+     * Edit an address from form POST action by modifying the database record
+     * @param int $person_id: the id of the person to which the address belongs
+     */
     public function edit($address_id) {
         $address = new \app\models\Address();
         $address = $address->get($address_id);
@@ -38,6 +53,7 @@ class AddressController extends \app\core\controller {
         $person = new \app\models\Person();
         $person = $person->get($address->person_id);
 
+        // If the form has been submitted, update the database record
         if (isset ($_POST['submit'])) {
             $address->setDescription($_POST['description']);
             $address->setStreet($_POST['street']);
@@ -47,22 +63,32 @@ class AddressController extends \app\core\controller {
             $address->setCountryCode($_POST['country_code']);
             $address->update();
 
+            // Redirect to the person's address page
             header("Location: /Address/index/$person->id");
         } else {
+            // Display the edit form
             $this->view('Address/edit', ['address'=>$address, 'person'=>$person]);
         }
     }
 
+    /**
+     * Show details of an address from form POST action by displaying the database record
+     * @param int $person_id: the id of the person to which the address(es) belongs
+     */
     public function details($person_id) {
         $address = new \app\models\Address();
-        $addressArray = $address->getAll($person_id);
-        $this->view('Address/details', $addressArray);
+        $addressArray = $address->getAll($person_id); // Get all addresses for the person
+        $this->view('Address/details', $addressArray); // Display the details page
     }
 
+    /**
+     * Delete an address from form POST action by deleting the database record
+     * @param int $person_id: the id of the person to which the address(es) belongs
+     */
     public function delete($person_id) {
         $person = new \app\models\Person();
-        $person->delete($person_id);
-        header("Location: /Address/index");
+        $person->delete($person_id); // Delete the address(es) for the person
+        header("Location: /Address/index"); // Redirect to the address page
     }
 }
 ?>
