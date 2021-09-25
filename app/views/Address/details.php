8d4f75349f873652b94ef3d6d9e650a36ff9b011
CryoTechnic <cryogen65@gmail.com>
2021-09-25 13:35:26 -0400

    Removed redundant details hyperlink

--- app/views/Address/details.php
+++ app/views/Address/details.php
@@ -13,27 +13,22 @@
                 <th>Province/State</th>
                 <th>postal/zip code</th>
                 <th>Country_code</th>
-
                 <?php 
                     foreach ($data as $addressRecord) 
                     echo "<tr> 
-                            <td>$addressRecord->address_id</td>
-                            <td>$addressRecord->person_id</td>
-                            <td>$addressRecord->description</td>
-                            <td>$addressRecord->street</td>
-                            <td>$addressRecord->city</td>
-                            <td>$addressRecord->province_state</td>
-                            <td>$addressRecord->postal_zip_code</td>
-                            <td>$addressRecord->country_code</td>
-                            <td><a href='/AddressController/details/$addressRecord->person_id'>Details</a></td>
+                            <td>$addressRecord->address_id |</td>
+                            <td>$addressRecord->person_id |</td>
+                            <td>$addressRecord->description |</td>
+                            <td>$addressRecord->street |</td>
+                            <td>$addressRecord->city |</td>
+                            <td>$addressRecord->province_state |</td>
+                            <td>$addressRecord->postal_zip_code |</td>
+                            <td>$addressRecord->country_code |</td>
                             <td><a href='/AddressController/delete/$addressRecord->person_id'>Delete</a></td>
                             <td><a href='/AddressController/edit/$addressRecord->address_id'>Edit</a></td>
                         </tr>"
                 ?>
             </table>
-           
-            <a href='/AddressController/index'>Back to list of addresses</a>
-
-
+            <a href='/Person/details/<?php echo $data[0]->person_id; ?>'>Back to list of addresses</a>
     </body>
 </html>
