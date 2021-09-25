0051198c7336f50e27df75d970c2d337780df0b0
CryoTechnic <cryogen65@gmail.com>
2021-09-25 13:49:39 -0400

    removed deprecated code

--- app/models/Country.php
+++ app/models/Country.php
@@ -11,29 +11,12 @@ namespace app\models;
      */ 
 
 class Country extends \app\core\Model{
-
     public $country_code;
     public $country_name;
 
     public function __construct($country_code='', $ocuntry_name=''){
         parent::__construct();
     }
-
-    // public function getCountryCode(){
-    //     return $this->country_code;
-    // }
-
-    // public function getCountryName(){
-    //     return $this->country_name;
-    // }
-
-    // public function setCountryCode($country_code){
-    //     $this->country_code = $country_code;
-    // }
-
-    // public function setCountryName($country_name){
-    //     $this->country_name = $country_name;
-    // }
 
     public function getAll(){
         $SQL = "SELECT * FROM country";
